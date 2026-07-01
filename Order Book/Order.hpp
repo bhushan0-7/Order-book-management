11b946b5322ba2d705c8522ed448fcd95a930214
bhushan0-7 <tallurivssnbhushan100@gmail.com>
2026-07-01 15:15:35 +0530

    Add order_type to Order struct and update constructor

--- Order Book/Order.hpp
+++ Order Book/Order.hpp
@@ -6,15 +6,15 @@ struct Order {
     string id; //unique identifier for a order
     string symbol; //ticker
     string type; //Buy or Sell order
+    string order_type;
     ll quantity;
     ll remaining_quantity;
     ll price;
     ll time_stamp;
 
-    //constructer for order
-    Order(string id,string symbol,string type,
+    Order(const string& id,const string& symbol,const string& order_type,const string& type,
         ll quantity,ll price,ll time):id(id),symbol(symbol),
-        type(type),quantity(quantity),price(price),time_stamp(time),remaining_quantity(quantity){}
+        order_type(order_type),type(type),quantity(quantity),price(price),time_stamp(time),remaining_quantity(quantity){}
 
 };
 
